eab350dd2601bc9ed3a21bbf9c0086de78f4815f
naughtysoda <ctainny@gmail.com>
2021-01-18 09:37:38 +0700

    28

--- myprojectnew/boards/models.py
+++ myprojectnew/boards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Board(models.Model):
-    name = models.CharField(max_length=30)
+    firstname = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     star = models.IntegerField(default=0,null=True)
 
