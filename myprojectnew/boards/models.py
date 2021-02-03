1e4999a6f11c989c0e5fa7852f31a7fcb146e828
naughtysoda <ctainny@gmail.com>
2021-02-03 09:33:58 +0700

    37

--- myprojectnew/boards/models.py
+++ myprojectnew/boards/models.py
@@ -8,17 +8,3 @@ class Board(models.Model):
 
     def __str__(self):
         return "{}-{}".format(self.firstname,self.star)
-
-# class Topic(models.Model):
-#     subject = models.CharField(max_length=255)
-#     last_updated = models.DateTimeField(auto_now_add=True)
-#     board = models.ForeignKey(Board, related_name='topics',on_delete=models.PROTECT)
-#     starter = models.ForeignKey(User, related_name='topics',on_delete=models.PROTECT)
-
-# class Post(models.Model):
-#     message = models.TextField(max_length=4000)
-#     topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.PROTECT)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(null=True)
-#     created_by = models.ForeignKey(User, related_name='posts',on_delete=models.PROTECT)
-#     updated_by = models.ForeignKey(User, null=True, related_name='+',on_delete=models.PROTECT)
